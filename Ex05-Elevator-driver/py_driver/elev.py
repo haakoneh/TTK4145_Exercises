a40e6eee5cc2a16fed79f8f68a2ff539b4bc4604
Chrislock1 <fredrikcch@msn.com>
2016-02-17 14:18:37 +0100

    Elev driver working

--- Ex05-Elevator-driver/py_driver/elev.py
+++ Ex05-Elevator-driver/py_driver/elev.py
@@ -1,27 +1,46 @@
-from io import IO
+from IO import io
 from channels import INPUT, OUTPUT
+from time import sleep
 
 class Elevator:
-	def _init_(self):
-		#self.moving = false
-		self.direction = OUTPUT.MOTOR_STOP
+	def __init__(self):
+		self.moving = False
+		self.direction = OUTPUT.MOTOR_DOWN
 		self.NUM_FLOORS = INPUT.NUM_FLOORS
 
 		for light in OUTPUT.LIGHTS:
 			if light != -1:
 				io.setBit(light, 0)
 
+	def setSpeed(self, speed):
+		if speed > 0:
+			self.direction = OUTPUT.MOTOR_UP
+		elif speed < 0:
+			self.direction = OUTPUT.MOTOR_DOWN
+		else:
+			self.stop()
 
+		io.setBit(OUTPUT.MOTORDIR, self.direction)
+		io.writeAnalog(OUTPUT.MOTOR, 2048+4*abs(speed))
+		self.moving = True
 
-		
+	def stop(self):
+		if not self.moving:
+			return
+		if self.direction is OUTPUT.MOTOR_DOWN:
+			io.setBit(OUTPUT.MOTORDIR, OUTPUT.MOTOR_UP)
+		else:
+			io.setBit(OUTPUT.MOTORDIR, OUTPUT.MOTOR_DOWN)
+		sleep(0.02)
+		io.write_analog(OUTPUT.MOTOR, 2048)
 
-
-	def setButtonLamp(self, lamp, value):
+	def setButtonLamp(self, floor, buttonType, value):
 		assert(floor >= 0), "ERR_ floor < 0"
 		assert(floor < self.NUM_FLOORS), "ERR_ floor > NUM_FLOORS"
-		assert(button >= 0), "ERR_ button < 0"
-		assert(button < self.NUM_FLOORS), "ERR_ button > NUM_FLOORS"
-		io.setBit(lamp, value)
+		assert(buttonType >= 0), "ERR_ buttonType < 0"
+		assert(buttonType < self.NUM_FLOORS - 1), "ERR_ buttonType > NUM_FLOORS"
+
+		io.setBit(INPUT.BUTTON_FLOORS[floor][buttonType], value)
 
 	def setMotorDirection(self, dir):
 		assert(dir in MOTOR_DIRECTION), "ERR: Invalid motor direction!"
@@ -44,11 +63,16 @@ class Elevator:
 
 	def getButtonSignal(self, button, floor):
 		assert(floor >= 0)
-		assert(floor < NUM_FLOORS)
-		if(io.readBit(OUTPUT.BUTTON_FLOORS[floor][button]))
+		assert(floor < self.NUM_FLOORS)
+		if(io.readBit(INPUT.BUTTON_FLOORS[floor][button])):
+			return 1
+		
+		else:
+			return 0
+
 			
 	def getFloorSensorSignal(self):
-		for index, sensor in enumerate(OUTPUT.SENSORS):
+		for index, sensor in enumerate(INPUT.SENSORS):
 			if io.readBit(sensor):
 				return index
 
@@ -59,11 +83,9 @@ class Elevator:
 		assert(value < 1), "ERR: door lamp value > 1"
 		io.setBit(OUTPUT.DOOROPEN, value)
 
-	def getStopSignal():
-		return io.readBit(STOP)
+	def getStopSignal(self):
+		return io.readBit(INPUT.STOP)
 
 	def getObstructionSignal():
-		return io.readBit(OBSTRUCTION)
-
-
+		return io.readBit(INPUT.OBSTRUCTION)
 
--- Ex05-Elevator-driver/py_driver/main.py
+++ Ex05-Elevator-driver/py_driver/main.py
@@ -1,5 +1,17 @@
-def  main():
-	print 'Hello world!'
+from elev import Elevator
 
+def main():
+	elev = Elevator()
+	elev.setSpeed(300)
+
+	while True:
+		if elev.getFloorSensorSignal() == 0:
+			elev.setSpeed(300)
+		elif elev.getFloorSensorSignal() == elev.NUM_FLOORS-1:
+			elev.setSpeed(-300)
+
+		if elev.getStopSignal():
+			elev.setSpeed(0)
+			break
 
 main()
